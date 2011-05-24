c10db818cdc48c8a98911cb627cc926a68fef418
卞中杰 <agasbzj@gmail.com>
2011-05-24 21:27:04 +0800

    修正my cards 为只匹配到银行级别而不匹配到每张卡的级别。

--- ekkk/NearbyTableViewController.m
+++ ekkk/NearbyTableViewController.m
@@ -63,41 +63,54 @@
         [allMyCards addObjectsFromArray:[dic valueForKey:@"cards"]];
     }
     
-    BOOL isSameBank = NO;   //是否拥有某个银行的卡，如果有，则跳过该银行其他的卡的判断，否则会重复添加。
+//    BOOL isSameBank = NO;   //是否拥有某个银行的卡，如果有，则跳过该银行其他的卡的判断，否则会重复添加。
     
+//    for (OneItem *item in allData) 
+//    {
+//            for (NSDictionary *bankDic in item.bank) 
+//            {
+//                NSArray *cardA = [bankDic valueForKey:@"card"];
+//                for (NSDictionary *cardD in cardA) 
+//                {
+//                    
+//                    
+//                    //传入的数据中的卡
+//                    NSString *str1 = [cardD valueForKey:@"card_name"];
+//                    
+//                    
+//                    //我的卡
+//                    for (NSString *str2 in allMyCards) 
+//                    {
+//                        if ([str1 isEqualToString:str2] == YES) 
+//                        {
+//                            [_dataArray addObject:item];
+//                            isSameBank = YES;
+//                            break;
+//                        }
+//                    }
+//                    if (isSameBank == YES) {
+//                        break;
+//                    }
+//
+//                }
+//                if (isSameBank == YES) {
+//                    isSameBank = NO;
+//                    continue;
+//                }
+//            }
+//    }
     for (OneItem *item in allData) 
     {
-            for (NSDictionary *bankDic in item.bank) 
-            {
-                NSArray *cardA = [bankDic valueForKey:@"card"];
-                for (NSDictionary *cardD in cardA) 
-                {
-                    
-                    
-                    //传入的数据中的卡
-                    NSString *str1 = [cardD valueForKey:@"card_name"];
-                    
-                    
-                    //我的卡
-                    for (NSString *str2 in allMyCards) 
-                    {
-                        if ([str1 isEqualToString:str2] == YES) 
-                        {
-                            [_dataArray addObject:item];
-                            isSameBank = YES;
-                            break;
-                        }
-                    }
-                    if (isSameBank == YES) {
-                        break;
-                    }
-
-                }
-                if (isSameBank == YES) {
-                    isSameBank = NO;
-                    continue;
+        for (NSDictionary *bankDic in item.bank) 
+        {
+            NSString *bankName = [bankDic valueForKey:@"bank_name"];
+            for (NSDictionary *dic in myCards) {
+                NSString *str2 = [dic valueForKey:@"bank_name"];
+                if ([bankName isEqualToString:str2]) {
+                    [_dataArray addObject:item];
                 }
             }
+        }
     }
 }
 
